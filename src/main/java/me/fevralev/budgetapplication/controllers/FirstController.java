0e9d2911709688b4cf91f7c13ae42c288f9a379e
afevralevtotyt <afevraleff@ya.ru>
2022-12-08 20:47:33 +0200

    done

--- src/main/java/me/fevralev/budgetapplication/controllers/FirstController.java
+++ src/main/java/me/fevralev/budgetapplication/controllers/FirstController.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FirstController {
+package me.fevralev.budgetapplication.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping
+    public String StartPage(){
+        return "Приложение запущено! ";
+    }
+    @GetMapping("/info")
+    public String Description(){
+        return "Студент Февралев Антон.\n" +
+                "Проект \"Книга рецептов бабы Любы\".\n" +
+                "Проект создан 08.12.2022." +
+                "\nДанный проект предназначен для начинающих кулинаров.\n" +
+                "Постоен на фреймворке Spring. Язык программирования Java v17. Собран сборщиком Maven";
+    }
 }
